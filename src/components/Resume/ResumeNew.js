14be6656d9b8d7b624ad6bc951f516b038f391d7
BulletStormXT <BulletStormXT@gmail.com>
2024-02-02 17:49:20 +0100

    0.30.1 - 2024-02-02 - 16-30
    Resume added

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,52 +1,51 @@
-import React, { useState, useEffect } from "react";
-import { Container, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
+import { Col, Container, Row } from "react-bootstrap";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
-import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
-
   return (
     <div>
-      <Container fluid className="resume-section">
+      <Container
+        fluid
+        className="resume-section"
+        style={{ textAlign: "left", width: "80%" }}
+      >
         <Particle />
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
-        </Row>
-
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
-        </Row>
-
-        <Row style={{ justifyContent: "center", position: "relative" }}>
-          <Button
-            variant="primary"
-            href={pdf}
-            target="_blank"
-            style={{ maxWidth: "250px" }}
-          >
-            <AiOutlineDownload />
-            &nbsp;Download CV
-          </Button>
+        <div>
+          <h5>Résumé </h5>
+          <br />
+          <h1 className="purple" style={{ fontVariantCaps: "" }}>
+            Mein CV .:. Lebenslauf .:. Curriculum Vitae
+          </h1>
+          <p>&nbsp;</p>
+        </div>
+        <Row style={{ justifyContent: "left", position: "relative" }}>
+          <Col md={6} className="d-flex justify-content-left">
+            <div>
+              <h3>Letzte Position</h3>
+              <h4 className="purple" style={{ fontVariant: "small-caps" }}>
+                Bundesagentur für Arbeit
+              </h4>
+              <p>
+                Als Fachassistent im Regionalen Infrastrukturmanagement (RIM)
+                war ich zuständig für die Bestandsaufnahme von IT- und
+                Mobilgeräten in allen Standorten von Thüringen. Aufgabe war es,
+                die Technik in jeder Agentur aufzunehmen, zu erfassen und im
+                dafür vorgesehenen IT-Bestandssystem abzugleichen.
+              </p>
+              <p>Dabei aufgebaute Kenntnisse</p>
+              <ul>
+                <li>IT-Assetmanagement</li>
+                <li>IT-Bestandserfassung</li>
+                <li>IT-Bestandsverwaltung</li>
+                <li>SAP-Produkte</li>
+                <li>Projekt-Management & Leadership</li>
+                <li>Hardware- und Software-Enrollment</li>
+              </ul>
+            </div>
+          </Col>
+          <Col md={6} className="d-flex justify-content-left">
+            <h2>Berufserfahrung</h2>
+          </Col>
         </Row>
       </Container>
     </div>
